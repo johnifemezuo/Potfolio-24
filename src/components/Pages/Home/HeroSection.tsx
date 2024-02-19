5e8dd781ba504d9fb112840ed02ad23a25ee2a81
johnifemezuo <johnifemezuoanayo@gmail.com>
2024-02-19 01:09:42 +0100

    update on animation

--- src/components/Pages/Home/HeroSection.tsx
+++ src/components/Pages/Home/HeroSection.tsx
@@ -1,7 +1,47 @@
-import { ArrowDownIcon, ArrowRight, HeroRoundedArt, PageContainer, RoundedArt } from "@/components";
+"use client";
+
+import {
+  ArrowDownIcon,
+  ArrowRight,
+  HeroRoundedArt,
+  PageContainer,
+  RoundedArt,
+} from "@/components";
 import Image from "next/image";
+import { gsap } from "gsap";
+import { useEffect, useLayoutEffect, useRef } from "react";
 
 export const HeroSection = () => {
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.fromTo(
+      "#heroTitle",
+      { duration: 1.6, opacity: 0, y: 70, ease: "Power3.easeOut" },
+      {
+        opacity: 1,
+        y: 0,
+        stagger: 0.2,
+      }
+    );
+
+    gsap.fromTo(
+      "#heroImg",
+      {
+        duration: 3,
+        opacity: 0,
+        y: 70,
+        scale: 0.4,
+        ease: "Power3.easeOut",
+        delay: 4,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+      }
+    );
+  }, []);
+
   return (
     <PageContainer>
       <div
@@ -10,7 +50,7 @@ export const HeroSection = () => {
       "
       >
         <div className="lg:-space-y-6 md:-space-y-6">
-          <h1 className="flex space-x-3 ">
+          <h1 id="heroTitle" className="flex space-x-3 ">
             <span className="text-white text-[3rem] sm:text-[4rem] lg:text-[8rem] xl:text-[10rem] md:text-[6rem]">
               I`m
             </span>{" "}
@@ -22,10 +62,16 @@ export const HeroSection = () => {
             className="flex--items space-x-4 md:space-x-6
           "
           >
-            <div className="rounded-full w-[66px] h-[66px] lg:w-[180px] md:w-[110px] md:h-[110px] lg:h-[180px] overflow-hidden">
+            <div
+              id="heroImg"
+              className="rounded-full w-[66px] h-[66px] lg:w-[180px] md:w-[110px] md:h-[110px] lg:h-[180px] overflow-hidden"
+            >
               <Image src="/images/me1.jpg" alt="me" width={200} height={200} />
             </div>
-            <h1 className=" text-white md:text-[6rem] text-[3rem] sm:text-[4rem] lg:text-[9rem] xl:text-[rem]">
+            <h1
+              id="heroTitle"
+              className=" text-white md:text-[6rem] text-[3rem] sm:text-[4rem] lg:text-[9rem] xl:text-[rem]"
+            >
               Ifemezuo
             </h1>
           </div>
@@ -43,7 +89,7 @@ export const HeroSection = () => {
           <div className="md:flex lg:max-w-[900px] justify-between mx-auto md:space-x-6 space-y-8 md:space-y-0">
             <div className="flex space-x-3">
               <span>
-                <RoundedArt className="w-[25px] md:w-[35px] h-[25px] md:h-[35px]" />
+                <RoundedArt className="w-[25px] md:w-[35px] h-[25px] md:h-[35px] spinnerLoop" />
               </span>
               <div className="space-y-3">
                 <h3 className="text-2xl text-white md:text-3xl xl:text-4xl">
@@ -58,7 +104,7 @@ export const HeroSection = () => {
 
             <div className="flex space-x-3">
               <span>
-                <RoundedArt className="w-[25px] md:w-[35px] h-[25px] md:h-[35px]" />
+                <RoundedArt className="w-[25px] md:w-[35px] h-[25px] md:h-[35px] spinnerLoop" />
               </span>
               <div className="space-y-3">
                 <h3 className="text-2xl text-white md:text-3xl xl:text-4xl">
@@ -83,7 +129,7 @@ export const HeroSection = () => {
 
         {/* Art */}
         <div className="absolute md:top-12 lg:top-24 md:right-12 lg:right-24 top-6 right-6">
-          <HeroRoundedArt className="xl:w-[500px] lg:w-[370px] lg:h-[370px] xl:h-[500px] w-[200px] h-[200px] md:w-[200px] md:h-[200px]" />
+          <HeroRoundedArt className="spinnerLoop xl:w-[500px] lg:w-[370px] lg:h-[370px] xl:h-[500px] w-[200px] h-[200px] md:w-[200px] md:h-[200px]" />
         </div>
         <div className="rounded-linear absolute bg-[#98843A] top-0 right-0 border w-[300px] h-[300px]"></div>
       </div>
